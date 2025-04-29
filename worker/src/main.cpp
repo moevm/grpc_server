d8bddcbaca4445c25fb462873e3edaa57cf26bec
RavenDenster <snaff2588@mail.ru>
2025-04-30 00:11:25 +0300

    fix format

--- worker/src/main.cpp
+++ worker/src/main.cpp
@@ -8,24 +8,24 @@ void print_usage(const std::string &program_name) {
             << std::endl;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc == 2 && std::string(argv[1]) == "--status") {
-        std::cout << "Worker status: OK" << std::endl;
-        return 0;
-    }
+int main(int argc, char *argv[]) {
+  if (argc == 2 && std::string(argv[1]) == "--status") {
+    std::cout << "Worker status: OK" << std::endl;
+    return 0;
+  }
 
-    if (argc < 3) {
-        print_usage(argv[0]);
-        return 1;
-    }
-    
-    try {
-        std::string hash = File::calculate_hash(argv[1], argv[2]);
-        std::cout << "Digest is: " << hash << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
+  if (argc < 3) {
+    print_usage(argv[0]);
+    return 1;
+  }
+
+  try {
+    std::string hash = File::calculate_hash(argv[1], argv[2]);
+    std::cout << "Digest is: " << hash << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
 
   return 0;
 }
