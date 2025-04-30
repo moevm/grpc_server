f9f953413cd7c4671df9f3249296772335b5aefa
Gorsky Kirill <kirillgorskiy@gmail.com>
2025-04-30 17:30:51 +0300

    fix converter.go codestyle

--- controller/pkg/converter/converter.go
+++ controller/pkg/converter/converter.go
@@ -2,23 +2,19 @@
 package converter
 
 import (
-	"errors";
-	"encoding/binary";
-)
-
-const (
-	intByteLen = 8
+	"encoding/binary"
+	"errors"
 )
 
 func IntToByteSlice(num int) []byte {
-    buf := make([]byte, 8)
-    binary.LittleEndian.PutUint64(buf, uint64(num))
-    return buf
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, uint64(num))
+	return buf
 }
 
 func ByteSliceToInt(slice []byte) (int, error) {
-    if len(slice) != 8 {
-        return 0, errors.New("invalid slice length")
-    }
-    return int(binary.LittleEndian.Uint64(slice)), nil
+	if len(slice) != 8 {
+		return 0, errors.New("invalid slice length")
+	}
+	return int(binary.LittleEndian.Uint64(slice)), nil
 }
