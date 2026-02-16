711d97d20fef70acd3b1faa1665db28238425e68
vovanbravin <viva.brovin@gmail.com>
2026-02-16 18:22:11 +0300

    add async and RPS

--- test/generate_https_http/main.py
+++ test/generate_https_http/main.py
@@ -1,20 +1,24 @@
 import json
 
-import requests
-import time
 import random
 import argparse
 import logging
+import asyncio
+import httpx
 
 
 class GenerateTraficHttpHttps:
 
     def __init__(self):
         self._config = {}
-        self._links = []
 
-    def _request(self, url: str):
-        return requests.get(url, timeout=(3, 5))
+    async def _request(self, url: str, client: httpx.AsyncClient):
+        try:
+            response = await client.get(url, timeout=10.0)
+            return response
+        except Exception as ex:
+            logging.error(f"Error request to {url}: {ex}")
+            return None
 
     def load_config_file(self, file_path: str):
         with open(file_path, 'r') as config_file:
@@ -27,22 +31,30 @@ class GenerateTraficHttpHttps:
     def set_option(self, option, value):
         self._config[option] = value
 
+    async def _generate_async(self):
+
+        async with httpx.AsyncClient() as client:
+            while True:
+                url = random.choice(self._config["root_urls"])
+
+                response = await self._request(url, client)
+
+                if response:
+                    logging.info(f"Request to {url} status {response.status_code}")
+
+                delay = 1 / self._config["RPS"]
+
+                await asyncio.sleep(delay)
+
     def generate(self):
 
-        while True:
-            url = random.choice(self._config["root_urls"])
-            try:
-                response = self._request(url)
-                logging.info("Request to {} status: {}".format(url, response.status_code))
-            except requests.exceptions.RequestException:
-                logging.warning("Error connecting to root url: {}".format(url))
+        asyncio.run(self._generate_async())
 
-            time.sleep(random.randrange(self._config["min_sleep"], self._config["max_sleep"]))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', metavar='-c', required=True, type=str, help='config file')
-    parser.add_argument('--log', metavar='-l', required=True, type=str, help = 'logging level')
+    parser.add_argument('--log', metavar='-l', required=True, type=str, help='logging level')
     args = parser.parse_args()
 
     level = getattr(logging, args.log.upper())
