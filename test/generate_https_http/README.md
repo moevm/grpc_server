81c0eb9c5b8f0cc213928bfefe4cfba33ac63011
vovanbravin <viva.brovin@gmail.com>
2026-02-25 22:10:35 +0300

    add tests and analysis status codes

--- test/generate_https_http/README.md
+++ test/generate_https_http/README.md
@@ -67,3 +67,30 @@ chmod +x generator.sh
     ]
 }
 ```
+
+# Logs
+All process logs are stored in the logs/ directory:
+```text
+logs/
+├── logs_0.log  # Process #0 log
+├── logs_1.log  # Process #1 log
+└── logs_2.log  # Process #2 log
+```
+
+Example logs_{i}.log:
+```text
+2026-02-25 18:27:44 - httpx - INFO - HTTP Request: GET http://www.cnn.com "HTTP/1.1 302 Found"
+2026-02-25 18:27:45 - httpx - INFO - HTTP Request: GET https://www.reddit.com "HTTP/1.1 403 Blocked"
+2026-02-25 18:27:45 - httpx - INFO - HTTP Request: GET https://thepiratebay.org "HTTP/1.1 302 Found"
+2026-02-25 18:27:45 - httpx - INFO - HTTP Request: GET https://wikipedia.org "HTTP/1.1 403 Forbidden"
+2026-02-25 18:27:45 - httpx - INFO - HTTP Request: GET http://4chan.org "HTTP/1.1 403 Forbidden"
+2026-02-25 18:27:45 - httpx - INFO - HTTP Request: GET https://medium.com "HTTP/1.1 403 Forbidden"
+2026-02-25 18:27:46 - httpx - INFO - HTTP Request: GET http://www.cnn.com "HTTP/1.1 302 Found"
+...
+# In end
+Url http://www.cnn.com status codes: 302: 8
+Url https://thepiratebay.org status codes: 302: 4
+Url https://www.reddit.com status codes: 403: 7
+Url https://wikipedia.org status codes: 403: 9
+Url http://4chan.org status codes: 403: 5
+```
--- test/generate_https_http/main.py
+++ test/generate_https_http/main.py
@@ -1,25 +1,49 @@
 import json
-
 import time
 import random
 import argparse
 import logging
 import asyncio
 import httpx
+import signal
+import sys
 
 
 class GenerateTrafficHttpHttps:
 
     def __init__(self):
         self._config = {}
+        self._stats = {}
+        self._is_running = True
+
+        signal.signal(signal.SIGINT, self._signal_handler)
+
+    def _signal_handler(self, sig, frame):
+        self._is_running = False
+
+    def _inc_stats_field(self, url: str, field: int) -> None:
+        if not self._stats.get(url):
+            self._stats[url] = {field: 1}
+        elif not self._stats[url].get(field):
+            self._stats[url][field] = 1
+        else:
+            self._stats[url][field] += 1
 
     async def _request(self, url: str, client: httpx.AsyncClient, timeout: float = 10.0):
         try:
             response = await client.get(url, timeout=timeout)
+            self._inc_stats_field(url, response.status_code)
             return response
-        except Exception:
-            logging.error(f"Error request to {url}")
-            return None
+        except httpx.ConnectError:
+            logging.error(f"Connect error for {url}")
+            self._inc_stats_field(url, "Connect error")
+        except httpx.ConnectTimeout:
+            logging.error(f"Connect timeout for {url}")
+            self._inc_stats_field(f"Connect timeout")
+        except Exception as ex:
+            logging.error(ex)
+
+        return None
 
     def load_config_file(self, file_path: str) -> bool:
 
@@ -41,7 +65,7 @@ class GenerateTrafficHttpHttps:
         delay = 1.0 / self._config["RPS"]
 
         async with httpx.AsyncClient() as client:
-            while True:
+            while self._is_running:
                 url = random.choice(self._config["root_urls"])
 
                 async def make_request(target_url: str):
@@ -52,29 +76,53 @@ class GenerateTrafficHttpHttps:
 
                 await asyncio.sleep(delay)
 
+        self._print_stats()
+
+    def _print_stats(self):
+        for url in self._stats:
+            print(f"Url {url} status codes:",
+                  ' '.join(f"{field}: {count}" for field, count in self._stats[url].items()))
+
     def generate(self, max_concurrent_requests: int):
 
         asyncio.run(self._generate_async(max_concurrent_requests))
+
+    def check_correct_config(self):
+        if not self._config.get("root_urls"):
+            logging.error("There is no root_urls field in the configuration file.")
+            return False
+        if not self._config.get("RPS"):
+            logging.error("There is no RPS field in the configuration file.")
+            return False
+
+        return True
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', metavar='-c', required=True, type=str, help='config file')
     parser.add_argument('--log', metavar='-l', required=True, type=str, help='logging level')
-    parser.add_argument('--max_concurrent', metavar='m', required=True, type=int, help='max concurent requests')
+    parser.add_argument('--max_concurent', metavar='m', required=True, type=int, help='max concurent requests')
     args = parser.parse_args()
 
     try:
         level = getattr(logging, args.log.upper())
     except Exception as ex:
-        print(ex)
+        logging.error(ex)
         return
 
-    logging.basicConfig(level=level)
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
     generator = GenerateTrafficHttpHttps()
 
     if not generator.load_config_file(args.config):
+        return
+
+    if not generator.check_correct_config():
         return
 
     generator.generate(args.max_concurent)
