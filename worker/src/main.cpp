8b7ac51d789810ffc5771afb4ef42de8e529b441
stepanrodimanov <stepanrodimanov133@icloud.com>
2026-03-18 20:12:47 +0300

    delete unix socket

--- worker/src/main.cpp
+++ worker/src/main.cpp
@@ -1,26 +1,19 @@
-#include "../include/md_calculator.hpp"
 #include "../include/metrics_collector.hpp"
 #include "../include/worker.hpp"
 
 #include <spdlog/spdlog.h>
 
-class HashWorker : public Worker {
+class FiltrWorker : public Worker {
   MetricsCollector metrics_collector;
 
 protected:
   void ProcessTask(const std::vector<char> &data) {
     metrics_collector.StartTask();
-
-    MDCalculator md_calculator("md5");
-    md_calculator.update((const unsigned char *)data.data(), data.size());
-    std::string hash = md_calculator.finalize();
-
-    SetFetchData(hash);
     metrics_collector.StopTask();
   }
 
 public:
-  HashWorker(const char *gateway_address, const char *gateway_port, uint64_t id)
+  FiltrWorker(const char *gateway_address, const char *gateway_port, uint64_t id)
       : Worker(id),
         metrics_collector(gateway_address, gateway_port,
                           ("worker-" + std::to_string(id)).c_str()) {}
@@ -47,7 +40,7 @@ int main() {
                gateway_port);
 
   try {
-    HashWorker worker(gateway_address, gateway_port, worker_id);
+    Worker worker(worker_id);
 
     bool test_mode = false;
     if (getenv("TEST_REQUEST_POLICY") != nullptr) {
@@ -78,9 +71,7 @@ int main() {
 
     worker.MainLoop();
 
-  } catch (WorkerException &e) {
-    spdlog::error(e.what());
-    return 1;
+
   } catch (std::exception &e) {
     spdlog::error("unhandled exception {}: {}", typeid(e).name(), e.what());
     return 1;
--- worker/src/worker.cpp
+++ worker/src/worker.cpp
@@ -1,17 +1,11 @@
 #include "../include/worker.hpp"
 
 #include "communication.grpc.pb.h"
-#include <arpa/inet.h>
-#include <cerrno>
-#include <endian.h>
 #include <grpcpp/grpcpp.h>
-#include <poll.h>
 #include <spdlog/spdlog.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 #include <thread>
-#include <unistd.h>
+#include <cstdlib>
+#include <ctime> 
 
 void Worker::LogStateChange(WorkerState new_state) {
   const char *state_names[] = {"BOOTING", "FREE", "BUSY", "SHUTTING_DOWN",
@@ -25,105 +19,6 @@ void Worker::SetState(WorkerState new_state) {
   if (state != new_state) {
     LogStateChange(new_state);
     state = new_state;
-  }
-}
-
-void Worker::ReadExact(int fd, void *buf, size_t n) {
-  size_t total = 0;
-  ssize_t r;
-
-  while (total < n) {
-    r = read(fd, (char *)buf + total, n - total);
-    if (r < 0) {
-      if (errno == EINTR) {
-        continue;
-      }
-      throw WorkerException(std::string("ReadExact failed with error ") +
-                            strerror(errno));
-    } else if (r == 0) {
-      throw WorkerException("ReadExact failed with Premature EOF");
-    }
-    total += r;
-  }
-}
-
-void Worker::WriteExact(int fd, const void *buf, size_t n) {
-  size_t total = 0;
-  ssize_t w;
-  while (total < n) {
-    w = write(fd, (const char *)buf + total, n - total);
-    if (w < 0) {
-      if (errno == EINTR) {
-        continue;
-      }
-      throw WorkerException(std::string("WriteExact failed with error ") +
-                            strerror(errno));
-    }
-    total += w;
-  }
-}
-
-void Worker::ReadMessage(int fd, std::string &msg) {
-  uint64_t length_le64 = 0;
-  Worker::ReadExact(fd, length_le64);
-  uint64_t length = le64toh(length_le64);
-  msg.resize(length);
-  Worker::ReadExact(fd, (void *)msg.data(), length);
-}
-
-void Worker::WriteMessage(int fd, const std::string &msg) {
-  uint64_t length_le64 = htole64(msg.size());
-  Worker::WriteExact(fd, length_le64);
-  Worker::WriteExact(fd, msg.data(), msg.size());
-}
-
-void Worker::SendPulse(PulseType type) {
-  int main_fd = 0;
-  try {
-    WorkerPulse pulse;
-    pulse.set_type(type);
-    pulse.set_worker_id(worker_id);
-    pulse.set_task_id(current_task_id);
-    pulse.set_next_pulse(EXPECTED_PULSE_TIME);
-
-    spdlog::info("Sending pulse... {{{}}}", pulse.ShortDebugString());
-
-    main_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (main_fd < 0)
-      throw WorkerException(std::string("socket: ") + strerror(errno));
-
-    sockaddr_un addr{.sun_family = AF_UNIX};
-    strncpy(addr.sun_path, SOCKET_DIR MAIN_SOCKET_NAME,
-            sizeof(addr.sun_path) - 1);
-
-    if (connect(main_fd, (sockaddr *)&addr, sizeof(addr))) {
-      throw WorkerException(std::string("connect: ") + strerror(errno));
-    }
-
-    WriteProtoMessage(main_fd, pulse);
-    spdlog::info("OK. Waiting for response");
-
-    pulse_interval =
-        MIN_PULSE_TIME + (rand() % (MAX_PULSE_TIME - MIN_PULSE_TIME + 1));
-    last_pulse_time = std::chrono::steady_clock::now();
-
-    PulseResponse response;
-    ReadProtoMessage(main_fd, response);
-
-    if (response.error() != CTRL_ERR_OK) {
-      throw WorkerException(ControllerError_Name(response.error()));
-    }
-    close(main_fd);
-
-    if (type == PULSE_REGISTER) {
-      worker_id = response.worker_id();
-    }
-
-    spdlog::info("Received {{{}}}", response.ShortDebugString());
-  } catch (const std::exception &e) {
-    close(main_fd);
-    SetState(WorkerState::ERROR);
-    throw WorkerException(std::string("SendPulse: ") + e.what());
   }
 }
 
@@ -147,6 +42,9 @@ void Worker::requestPolicyFromController() {
     if (policy.config_version() == 0) {
       spdlog::info("Policy unchanged");
       return;
+    }
+    else {
+      current_config_version = policy.config_version();
     }
 
     spdlog::info("Policy received");
@@ -226,94 +124,6 @@ Worker::Worker(uint64_t id) : worker_id(id), state(WorkerState::FREE) {
 Worker::~Worker() {
   SetState(WorkerState::SHUTTING_DOWN);
   spdlog::info("Worker {} shutting down", worker_id);
-}
-
-int Worker::GetPulseTimeout() {
-  using namespace std::chrono;
-  return (pulse_interval -
-          duration_cast<seconds>(steady_clock::now() - last_pulse_time)
-              .count()) *
-         1000;
-}
-
-void Worker::HandleRestartControlMessage(WorkerResponse &resp) {
-  SetState(WorkerState::SHUTTING_DOWN);
-  resp.set_error(WORKER_ERR_OK);
-}
-
-void Worker::HandleFetchControlMessage(WorkerResponse &resp) {
-  if (fetch_data.size() == 0) {
-    resp.set_error(WORKER_ERR_NO_FETCH);
-    return;
-  }
-
-  extra_data = std::move(fetch_data);
-  SetState(WorkerState::FREE);
-}
-
-void Worker::ProcessTask_Static(Worker *worker, const std::vector<char> &data) {
-  worker->SetState(WorkerState::BUSY);
-  worker->ProcessTask(data);
-  worker->SetState(WorkerState::FREE);
-}
-
-void Worker::HandleSetTaskControlMessage(const ControlMsg &msg,
-                                         WorkerResponse &resp,
-                                         const std::vector<char> &extra) {
-  if (GetState() != WorkerState::FREE) {
-    resp.set_error(WORKER_ERR_BUSY);
-    return;
-  }
-
-  current_task_id = msg.task_id();
-  std::thread(ProcessTask_Static, this, extra).detach();
-}
-
-void Worker::HandleGetStatusControlMessage(WorkerResponse &resp) {}
-
-void Worker::HandleControlMessage(int client_fd) {
-  WorkerResponse response;
-  response.set_task_id(current_task_id);
-  response.set_error(WORKER_ERR_OK);
-
-  try {
-    ControlMsg msg;
-    ReadProtoMessage(client_fd, msg);
-
-    spdlog::info("Recieved {{{}}}", msg.ShortDebugString());
-
-    std::vector<char> extra(msg.extra_size());
-    ReadExact(client_fd, extra.data(), extra.size());
-
-    switch (msg.type()) {
-    case CTRL_RESTART:
-      HandleRestartControlMessage(response);
-      break;
-    case CTRL_FETCH:
-      HandleFetchControlMessage(response);
-      break;
-    case CTRL_SET_TASK:
-      HandleSetTaskControlMessage(msg, response, extra);
-      break;
-    case CTRL_GET_STATUS:
-      HandleGetStatusControlMessage(response);
-      break;
-    default:
-      response.set_error(WORKER_ERR_FAILED);
-      break;
-    }
-  } catch (const std::exception &e) {
-    response.set_error(WORKER_ERR_FAILED);
-  }
-
-  response.set_extra_size(extra_data.size());
-  WriteProtoMessage(client_fd, response);
-
-  if (extra_data.size() != 0) {
-    WriteExact(client_fd, extra_data.data(), extra_data.size());
-    extra_data.clear();
-  }
-  spdlog::info("Sent {{{}}}", response.ShortDebugString());
 }
 
 void Worker::MainLoop() {
