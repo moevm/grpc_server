2d652aa21ae624255d641ebc04695cc011dbc449
LapshinAE0 <sasha_lapshin_2006@mail.ru>
2026-03-31 10:58:13 +0300

    linter

--- worker/include/dpdk_filter/filtr_packets.h
+++ worker/include/dpdk_filter/filtr_packets.h
@@ -1,9 +1,9 @@
 #ifndef FILTR_PAK_H
 #define FILTR_PAK_H
 
+#include "pars_packets.h"
 #include <rte_mbuf.h>
 #include <stdint.h>
-#include "pars_packets.h"
 
 bool main_filtring(struct info_of_pakage *info_pac);
 
