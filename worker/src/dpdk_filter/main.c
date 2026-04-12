8283bfb15efb557b092d49be31466ba5819344e3
LapshinAE0 <sasha_lapshin_2006@mail.ru>
2026-04-12 16:02:14 +0300

    add send to port in

--- worker/src/dpdk_filter/main.c
+++ worker/src/dpdk_filter/main.c
@@ -18,11 +18,11 @@ static void signal_handler(int signum) {
   }
 }
 
-void forward_tap_to_out(struct net_port *port_exception, struct net_port *port_in, uint16_t queue_number) {
+void forward_to_out(struct net_port *incoming_port, struct net_port *outgoing_port, uint16_t queue_number) {
     struct rte_mbuf *tap_pkts[32];
-    uint16_t nb_tap = rte_eth_rx_burst(port_exception->port_id, queue_number, tap_pkts, 32);
+    uint16_t nb_tap = rte_eth_rx_burst(incoming_port->port_id, queue_number, tap_pkts, 32);
     for (int i = 0; i < nb_tap; i++) {
-        int ret = rte_eth_tx_burst(port_in->port_id, queue_number, &tap_pkts[i], 1);
+        int ret = rte_eth_tx_burst(outgoing_port->port_id, queue_number, &tap_pkts[i], 1);
         if (ret < 1) {
           printf("[ERROR] Failed to send packet\n");
           // PLUG (to be added later) - need to add processing for this case
@@ -109,8 +109,9 @@ int main(int argc, char **argv) {
          port_in->port_id, port_out->port_id);
 
   while (running) {
-    forward_tap_to_out(port_exception, port_in, queue_number);
+    forward_to_out(port_exception, port_in, queue_number);
     pakage_processing(port_in, port_out, port_exception, queue_number, nb_pkts, pkts, &policy);
+    forward_to_out(port_out, port_in, queue_number);
   }
 
   // function for save cache info if need
