e8bcdee00ab0b9f67d9b93a74d9ad358dcec763d
stepanrodimanov <stepanrodimanov133@icloud.com>
2026-05-07 14:20:49 +0300

    feat: add ip in worker

--- worker/include/worker.hpp
+++ worker/include/worker.hpp
@@ -4,7 +4,8 @@
 #include "communication.grpc.pb.h"
 #include "communication.pb.h"
 extern "C" {
-#include "dpdk_filter/dns_cache.h"
+#include "dpdk_filter/domain_cache.h"
+#include "dpdk_filter/ip_cache.h"
 #include "dpdk_filter/filtr_packets.h"
 #include "dpdk_filter/net_port.h"
 #include "dpdk_filter/proc_packets.h"
